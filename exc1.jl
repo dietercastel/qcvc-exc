9b3bd5a40d3e70e4bd321066c00341bcf496ab73
Dieter Castel <dietercastel@gmail.com>
2019-04-29 11:42:30 +0200

    Create exc1.jl

---
+++ exc1.jl
@@ -0,0 +1,14 @@
+# Let H be the Haddamard matrix (for the haddamard quantum gate):
+# Verify that H * H = I where I is the 2x2 identity matrix
+
+s = UniformScaling(1/sqrt(2))
+
+println(s)
+
+H = s * [1 1; 1 -1]
+
+println(H)
+
+Q = H * H
+
+println(Q)
